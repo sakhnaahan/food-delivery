a991940701b1c28368ef0bfa018631ccc0d93356
sakhnaahan <saikhnaahan@gmail.com>
2025-10-27 16:55:59 +0800

    fix login ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <header>
-        <a href="">Logo</a>
-        
-      </header>
-    </>
-  )
+  return <>hello</>
 }
