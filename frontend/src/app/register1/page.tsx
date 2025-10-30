e99ad6fd4d6112c80faf9c42cbe4c88b07791f55
sakhnaahan <saikhnaahan@gmail.com>
2025-10-30 23:37:23 +0800

    t

--- frontend/src/app/register1/page.tsx
+++ frontend/src/app/register1/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div className="w-full max-w-[1440px] bg-white h-screen flex items-center justify-center gap-10">
       <div className="max-w-[416px] gap-6 flex flex-col">
-        <div className="px-[16px] py-[8px] relative w-full h-[32px]">
+        <div className="px-4 py-2 relative w-full h-8">
           <img
             src="/btn_login.svg"
             alt="btn_login"
@@ -13,24 +13,24 @@ const page = () => {
           />
         </div>
         <div className="self-stretch">
-          <h3 className="text-[#09090B] text-[24px] font-semibold leading-[32px]">
+          <h3 className="text-[#09090B] text-[24px] font-semibold leading-8">
             Create your account
           </h3>
-          <p className="text-[16px] font-[400] leading-[24px] text-[#71717A]">
+          <p className="text-[16px] font-normal leading-6 text-[#71717A]">
             Sign up to explore your favorite dishes.
           </p>
         </div>
         <input
           type="email"
           placeholder="Enter your email address"
-          className="w-full rounded-[6px] border-1 border-[#E4E4E7] px-2 py-1.5"
+          className="w-full rounded-md border border-[#E4E4E7] px-2 py-1.5"
         />
-        <button className="w-full rounded-[6px] bg-gray-800/20 text-white py-2 px-4">
+        <button className="w-full rounded-md bg-gray-800/20 text-white py-2 px-4">
           Let's Go
         </button>
-        <p className="text-[#71717A] text-[16px] font-[400]">
+        <p className="text-[#71717A] text-[16px] font-normal">
           Already have an account?
-          <span className="text-[#2563EB] text-[16px] font-[400] leading-[24px] ml-1">
+          <span className="text-[#2563EB] text-[16px] font-normal leading-6 ml-1">
             Log in
           </span>
         </p>
